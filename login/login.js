be8752d43b66d202ff84cbd7074cdf11ac5dd696
Hume <925346531@qq.com>
2020-11-20 22:05:17 +0800

    Big Event项目，进度到删除功能

---
+++ login/login.js
@@ -0,0 +1,89 @@
+// -------------------------------------------------------切换页面
+$("#goto-register").on("click", function () {
+    $("#login").hide();
+    $("#register").show();
+})
+$("#goto-login").on("click", function () {
+    $("#login").show();
+    $("#register").hide();
+})
+// -------------------------------------------------------注册功能
+$(".layui-form").on("submit", function (e) {
+    e.preventDefault();
+    var params = $(this).serialize();
+    // 正则校验用户注册信息
+    var form = layui.form;
+    form.verify({
+        // -------------正则校验
+        username: function (val) {
+            if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(val)) {
+                return '用户名不能有特殊字符';
+            }
+            if (/(^\_)|(\__)|(\_+$)/.test(val)) {
+                return '用户名首尾不能出现下划线\'_\'';
+            }
+            if (/^\d+\d+\d$/.test(val)) {
+                return '用户名不能全为数字';
+            }
+            if (new RegExp("^[a-zA-Z0-9]{6,12}$").test(val) == false) {
+                return '用户名6-12个字符';
+            }
+            // if (/^[0-9]/.test(val)) {
+            //     return '用户名不能以数字开头';
+            // }
+        },
+        // layui校验的两种方式都可以
+        pass: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        same: function (val) {
+            var pwd = $("#pwd").val()
+            if (pwd !== val) {
+                return '两次密码不一致'
+            }
+        }
+    })
+    $.ajax({
+        type: "post",
+        url: "http://ajax.frontend.itheima.net/api/reguser",
+        data: params,
+        success: function (res) {
+            // 弹窗显示res.message
+            if (res.status == 0) {
+                layer.msg(res.message);
+                $("#login").show();
+                $("#register").hide();
+                // console.log(res);
+                // 输入框清空
+                $("#register form")[0].reset();
+            }
+        }
+    })
+    // post方式：
+    // $.post('http://ajax.frontend.itheima.net/api/reguser', params, function (res) {
+    //     console.log(res);
+    // })
+    
+})
+// -----------------------------------------------------------登录
+$(".layui-form").on("submit", function (e) {
+    e.preventDefault();
+    var params = $(this).serialize();
+    $.ajax({
+        type: "post",
+        url: "http://ajax.frontend.itheima.net/api/login",
+        data: params,
+        success: function (res) {
+            // 登陆成功或失败，弹窗显示
+            layer.msg(res.message);
+            if (res.status == 0) {
+                // console.log(res);
+                //提交成功后，把获取到的token手动保存在本地
+                localStorage.setItem("token", res.token);
+                location.href = "/index.html";
+            }
+        }
+    })
+})
+
